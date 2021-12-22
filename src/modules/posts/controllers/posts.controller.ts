343211c6326b7be73203b69026a8773ad0627982
Mahmoud Naguib <nagooba@gmail.com>
2021-12-22 23:12:26 +0200

    Fix posts controller

--- src/modules/posts/controllers/posts.controller.ts
+++ src/modules/posts/controllers/posts.controller.ts
@@ -2,17 +2,18 @@ import { Controller, Get, Param, Request } from '@nestjs/common';
 import { Pagination } from '../../../paginate';
 /////////////////////////////////////////////////
 import { PostModel as Model } from '../models/post.model';
-import { PostService } from '../services/post.service';
-import { PostResource } from '../resources/post.resource';
+import { PostService as Service } from '../services/post.service';
+import { PostResource as Resource } from '../resources/post.resource';
 
 @Controller('api/posts')
 export class PostsController {
-  constructor(private readonly service: PostService) {}
+  constructor(private readonly service: Service) {}
+
   @Get()
   async index(@Request() request): Promise<Pagination<Model>> {
     const rows = await this.service.findAllWithPaginate(request);
     rows.data = rows.data.map(function (item) {
-      return new PostResource(item).toArray();
+      return new Resource(item).toArray();
     });
     return rows;
   }
@@ -20,6 +21,6 @@ export class PostsController {
   @Get(':id')
   async show(@Param('id') id) {
     const row = await this.service.findOne(id);
-    return { data: new PostResource(row).toArray() };
+    return { data: new Resource(row).toArray() };
   }
 }
