727dac91d8654910c4073d681b04d861b9ca4025
Mahmoud Naguib <nagooba@gmail.com>
2022-03-20 01:08:33 +0200

    Fix image upload

--- src/modules/posts/services/post.service.ts
+++ src/modules/posts/services/post.service.ts
@@ -70,10 +70,7 @@ export class PostService {
     }
     /////////// resize image
     if (record.image != undefined) {
-      const image = await ResizeImage.resize(
-        record.image.path,
-        this.imageSizes,
-      );
+      const image = ResizeImage.resize(record.image.path, this.imageSizes);
       if (image) {
         record.image = image;
       }
